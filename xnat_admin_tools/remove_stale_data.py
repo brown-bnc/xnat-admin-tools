67d0d0b853610dc1c2d7f686e0006ca50b969f17
Isabel Restrepo <isabelrestre@gmail.com>
2021-11-10 16:13:36 -0500

    feat: add function that removes data to satisfy constrains on disk usage

--- xnat_admin_tools/remove_stale_data.py
+++ xnat_admin_tools/remove_stale_data.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime, timedelta
 
+import psutil
 import pyxnat
 import typer
 from dotenv import load_dotenv
@@ -12,6 +13,8 @@ app = typer.Typer()
 
 @app.command()
 def remove_stale(days: int):
+    """Removes participant data form an XNAT host that is older
+    than the specified number of days"""
     typer.echo(f"Searching sessions older than {days} days")
     xnat_host = os.environ.get("XNAT_RELAY_HOST", "")
     xnat_user = os.environ.get("XNAT_RELAY_USER", "")
@@ -36,10 +39,26 @@ def remove_stale(days: int):
     typer.echo(f"Found {len(to_delete)} sessions")
     for session in to_delete:
         typer.echo(f"Deleting {session}")
-        p = connection.select.project(session["project"])
-        p.subject(session["subject_id"]).delete()
+        # p = connection.select.project(session["project"])
+        # p.subject(session["subject_id"]).delete()
 
     connection.disconnect()
+
+
+@app.command()
+def enforce_disk_usage(
+    path: str, percent: int, min_days: int, max_days: int, step_days: int = -5
+):
+    """Check the percent usage of a location,
+    then remove stale projects until target percent min_days is hit,
+    whichever comes first"""
+    for days in range(max_days, min_days, step_days):
+        disk_usage = psutil.disk_usage(path).percent
+        typer.echo(f"Current disk usage for {path} is {disk_usage}")
+        if disk_usage > percent:
+            remove_stale(days)
+        else:
+            break
 
 
 def main():
