3cc7456f8719c4552d95cc7e8aa0d9b8739114a8
Isabel Restrepo <isabelrestre@gmail.com>
2021-11-23 10:52:28 -0500

    refactor: move the connection to the outer most loop

--- xnat_admin_tools/remove_stale_data.py
+++ xnat_admin_tools/remove_stale_data.py
@@ -12,14 +12,10 @@ app = typer.Typer()
 
 
 @app.command()
-def remove_stale(days: int):
+def remove_stale(connection: pyxnat.Interface, days: int):
     """Removes participant data form an XNAT host that is older
     than the specified number of days"""
     typer.echo(f"Searching sessions older than {days} days")
-    xnat_host = os.environ.get("XNAT_RELAY_HOST", "")
-    xnat_user = os.environ.get("XNAT_RELAY_USER", "")
-    xnat_pass = os.environ.get("XNAT_RELAY_PASS", "")
-    connection = pyxnat.Interface(server=xnat_host, user=xnat_user, password=xnat_pass)
     now = datetime.now()
     cutoff = now - timedelta(days=days)
     date_time = cutoff.strftime("%Y-%m-%d, %H:%M:%S")
@@ -29,6 +25,7 @@ def remove_stale(days: int):
             [
                 "xnat:mrSessionData/PROJECT",
                 "xnat:mrSessionData/SUBJECT_ID",
+                "xnat:mrSessionData/SUBJECT_LABEL",
                 "xnat:mrSessionData/INSERT_DATE",
             ],
         )
@@ -45,23 +42,30 @@ def remove_stale(days: int):
             session["subject_id"]
         ).exists(), "Participant data was not deleted"
 
-    connection.disconnect()
-
 
 @app.command()
 def enforce_disk_usage(
     path: str, percent: int, min_days: int, max_days: int, step_days: int = -5
 ):
-    """Check the percent usage of a location,
+    """Checks the percent usage of a location,
     then remove stale projects until target percent min_days is hit,
     whichever comes first"""
+
+    xnat_host = os.environ.get("XNAT_RELAY_HOST", "https://xrelay.bnc.brown.edu")
+    xnat_user = os.environ.get("XNAT_RELAY_USER", "admin")
+    xnat_pass = os.environ.get("XNAT_RELAY_PASS", "")
+
+    connection = pyxnat.Interface(server=xnat_host, user=xnat_user, password=xnat_pass)
+
     for days in range(max_days, min_days, step_days):
         disk_usage = psutil.disk_usage(path).percent
         typer.echo(f"Current disk usage for {path} is {disk_usage}")
         if disk_usage > percent:
-            remove_stale(days)
+            remove_stale(connection, days)
         else:
             break
+
+    connection.disconnect()
 
 
 def main():
