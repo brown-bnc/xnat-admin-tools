9e4bcd7b55ff00ec16d42e39ad973913794dc186
Ford McDonald <ford_mcdonald@brown.edu>
2023-06-16 13:47:02 -0400

    style(renew_xnat_tokens.py): ran black and isort locally to resolve linter errors

--- xnat_admin_tools/renew_xnat_tokens.py
+++ xnat_admin_tools/renew_xnat_tokens.py
@@ -1,9 +1,11 @@
 import requests
 import typer
 from requests.auth import HTTPBasicAuth
+
 from xnat_admin_tools.create_new_projects import set_xsync_credentials
 
 app = typer.Typer()
+
 
 def fetch_all_projects(xserver_host, xserver_user, xserver_pass):
     # GET all projects from xnat remote server
@@ -17,30 +19,31 @@ def fetch_all_projects(xserver_host, xserver_user, xserver_pass):
     )
     return response.json()
 
+
 # Iterates over all projects, applying updated token
 @app.command()
-def renew_xnat_tokens(            
+def renew_xnat_tokens(
     xrelay_host: str,
     xrelay_user: str,
     xrelay_pass: str,
     xserver_host: str,
     xserver_user: str,
-    xserver_pass: str
+    xserver_pass: str,
 ):
     projects = fetch_all_projects(xserver_host, xserver_user, xserver_pass)
 
-    for project in projects['ResultSet']['Result']:
-        
-        project_id = project['ID'] 
+    for project in projects["ResultSet"]["Result"]:
+        project_id = project["ID"]
         set_xsync_credentials(
-            xrelay_host, 
+            xrelay_host,
             xrelay_user,
             xrelay_pass,
             xserver_host,
             xserver_user,
             xserver_pass,
-            project_id
+            project_id,
         )
+
 
 if __name__ == "__main__":
     app()
