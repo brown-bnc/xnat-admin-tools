a6f77a16b4fd35cf7522700780770e199a73c0bb
Ford McDonald <ford_mcdonald@brown.edu>
2024-01-24 16:22:14 -0500

    feat: replicate projects script and refactor common functions into utilities folder

--- xnat_admin_tools/renew_xnat_tokens.py
+++ xnat_admin_tools/renew_xnat_tokens.py
@@ -1,23 +1,7 @@
-import requests
 import typer
-from requests.auth import HTTPBasicAuth
-
-from xnat_admin_tools.create_new_projects import set_xsync_credentials
+from xnat_admin_tools.utils.common import set_xsync_credentials, fetch_all_projects
 
 app = typer.Typer()
-
-
-def fetch_all_projects(xserver_host, xserver_user, xserver_pass):
-    # GET all projects from xnat remote server
-    basic = HTTPBasicAuth(xserver_user, xserver_pass)
-    get_url = xserver_host + "/data/projects"
-    response = requests.request(
-        "GET",
-        get_url,
-        headers={"Content-Type": "application/json"},
-        auth=basic,
-    )
-    return response.json()
 
 
 # Iterates over all projects, applying updated token
